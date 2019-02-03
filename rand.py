7f4f6c832ad3493b3dde1a2633a1596de7cd34e7
ryo-simon_mf <ryosimon1108@gmail.com>
2019-02-03 23:48:55 +0900

    add rand.py

---
+++ rand.py
@@ -0,0 +1,17 @@
+import random
+
+number = random.randint(1,6)
+#output 1 to 6
+
+if number == 1:
+  print("1")
+if number == 2:
+  print("2")
+if number == 3:
+  print("3")
+if number == 4:
+  print("4")
+if number == 5:
+  print("5")
+if number == 6:
+  print("6")
